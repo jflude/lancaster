738cb4408b3069f014d9e3008eb2a1f84dcdf2b5
Justin Flude <jflude@peak6.com>
2014-07-23 15:10:03 -0500

    test storage in Go a little bit more

--- go/pub/testpub.go
+++ go/pub/testpub.go
@@ -2,16 +2,21 @@ package main
 
 // #include "../../status.h"
 // #include "../../storage.h"
+// #include "../../error.h"
 // #cgo LDFLAGS: -L../.. -lcachester -lrt
 import "C"
 import (
 	"log"
+	"os"
 )
 
 func main() {
 	log.Println("stuff")
-	cs := C.CString("./foo.store")
+	cs := C.CString(os.Args[1])
 	var store C.storage_handle
-	status := C.storage_create(&store, cs, 0, 0, 1000, 100)
+	status := C.storage_create(&store, cs, 128, 0, 1000, 100)
+	log.Println(store, status)
+	C.error_report_fatal()
+	status = C.storage_reset(store)
 	log.Println(store, status)
 }
