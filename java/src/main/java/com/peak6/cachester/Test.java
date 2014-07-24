689f1bf635d4c98ba2a7a3a0ea58f09b59281356
David Budworth <dbudworth@peak6.com>
2014-07-24 06:49:19 -0500

    nearling working java version, need libcachester.so to link with librt

--- java/src/main/java/com/peak6/cachester/Test.java
+++ java/src/main/java/com/peak6/cachester/Test.java
@@ -2,6 +2,7 @@ package com.peak6.cachester;
 
 import com.sun.jna.Library;
 import com.sun.jna.Native;
+import com.sun.jna.Pointer;
 import com.sun.jna.ptr.PointerByReference;
 
 /**
@@ -10,15 +11,34 @@ import com.sun.jna.ptr.PointerByReference;
 public class Test {
     public interface Cachester extends Library {
         Cachester INSTANCE = (Cachester)
-                Native.loadLibrary("/home/dbudworth/gits/cachester/cachester",
+                Native.loadLibrary("/home/dbudworth/gits/cachester/libcachester.so",
                         Cachester.class);
 
         int storage_create(PointerByReference store, String path, int queue, int dunno, int records, int recordSz);
+
+        int storage_reset(Pointer p);
+
+        String error_last_desc();
     }
 
     public static void main(String[] args) {
-        System.out.println(System.getProperty("user.dir"));
+        String path = "";
+        if (args.length > 0) {
+            path = args[0];
+        }
+        System.out.println("Using path: " + path);
         PointerByReference pref = new PointerByReference();
-        System.out.println("Result: " + Cachester.INSTANCE.storage_create(pref, "./foo.store", 128, 0, 1000, 1));
+        System.out.println("Create");
+        exitOnError(Cachester.INSTANCE.storage_create(pref, path, 128, 0, 1000, 100));
+        System.out.println("Reset");
+        exitOnError(Cachester.INSTANCE.storage_reset(pref.getValue()));
+    }
+
+    static void exitOnError(int status) {
+        if (status == 0) {
+            return;
+        }
+        System.err.println("Error: " + Cachester.INSTANCE.error_last_desc());
+        System.exit(1);
     }
 }
