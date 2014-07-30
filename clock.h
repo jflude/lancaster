b929d24329bc4d11b07eb74da29daed95834580f
Justin Flude <jflude@peak6.com>
2014-07-30 18:33:31 -0500

    fix clock.h for C++ #includes

--- clock.h
+++ clock.h
@@ -5,9 +5,17 @@
 
 #include "status.h"
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 typedef long microsec_t;
 
 status clock_sleep(microsec_t usec);
 status clock_time(microsec_t* pusec);
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif
