a0f2e87b9246ee3295fd16f828b57b0fcd70ef9f
David Budworth <dbudworth@peak6.com>
2014-08-01 13:35:25 -0500

    quotester now has find vs tail

--- go/quotester/quotester.go
+++ go/quotester/quotester.go
@@ -8,46 +8,44 @@ package main
 import "C"
 import (
 	"errors"
+	"flag"
 	"fmt"
-	"github.com/willf/bitset"
 	"log"
 	"os"
 )
 
 var store C.storage_handle
-var qCapacity C.uint
-var rSize C.uint
-var hasWatch = false
-var watchKeys []string
-var watchBits bitset.BitSet
+var tail bool
 
+func init() {
+	flag.BoolVar(&tail, "t", false, "Tail feed")
+}
 func main() {
+	flag.Parse()
+	args := flag.Args()
 	var cs *C.char
-	if len(os.Args) > 1 {
-		cs = C.CString(os.Args[1])
-	} else {
-		log.Fatal("Must specify file")
+	if len(args) == 0 {
+		flag.Usage()
+		fmt.Fprintln(os.Stderr, "Must specify a file")
+		os.Exit(1)
 	}
-	if len(os.Args) > 2 {
-		hasWatch = true
-		for _, a := range os.Args[2:] {
-			watchKeys = append(watchKeys, a)
-		}
-	}
+	cs = C.CString(args[0])
 	if err := err(C.storage_open(&store, cs)); err != nil {
 		log.Fatal("Failed to open store", err)
 	}
 	defer func() { C.storage_destroy(&store) }()
-	qCapacity = C.storage_get_queue_capacity(store)
-	a := C.storage_get_record_size(store)
-	b := C.storage_get_value_size(store)
-	c := C.storage_get_value_offset(store)
-	log.Println("rs", a, "vs", b, "vo", c)
-	log.Println("Queue size:", qCapacity)
-	if watchKeys != nil {
-		log.Println("watchkeys:", watchKeys)
+
+	args = args[1:]
+
+	if tail {
+		tailQuotes(args)
+	} else {
+		if len(args) < 1 {
+			flag.Usage()
+		} else {
+			findQuotes(args)
+		}
 	}
-	runDirect()
 }
 
 func err(status C.status) error {
