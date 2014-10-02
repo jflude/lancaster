6919fc85dbac1f655ac13c6983dee4ee3eefcb0e
Mike Vander Pluym <mvanderpluym@peak6.com>
2014-10-02 11:14:52 -0500

    Changing names of variables in cachester's GOOP.mk to avoid env name conflicts with PricingServerII's GOOP.mk.

--- go/GOOP.mk
+++ go/GOOP.mk
@@ -2,14 +2,18 @@ SELF_DIR := $(shell readlink -e $(dir $(lastword $(MAKEFILE_LIST))))
 CACHESTER_DIR := $(shell readlink -e $(SELF_DIR)/"..")
 SCRIPTS_DIR := "$(shell readlink -e $(CACHESTER_DIR)/scripts)"
 
-VENDOR_DIR := $(SELF_DIR)/.vendor
-GOOP_DIR=$(shell readlink -e $(VENDOR_DIR)/..)
-export GOPATH:=$(VENDOR_DIR):$(GOPATH)
-export PATH:=$(VENDOR_DIR)/bin:$(PATH)
+CACHESTER_VENDOR_DIR := $(SELF_DIR)/.vendor
+CACHESTER_GOOP_DIR=$(shell readlink -e $(CACHESTER_VENDOR_DIR)/..)
+export GOPATH:=$(CACHESTER_VENDOR_DIR):$(GOPATH)
+export PATH:=$(CACHESTER_VENDOR_DIR)/bin:$(PATH)
 
 .ensurePrivateGithubSSH:
 	git config --global url.ssh://git@github.peak6.net/.insteadOf https://github.peak6.net
 
 .getGoop: .ensurePrivateGithubSSH
 	go get github.com/nitrous-io/goop
-	mkdir -p $(VENDOR_DIR)
+	mkdir -p $(CACHESTER_VENDOR_DIR)
+
+.installGoop: .getGoop
+	cd $(CACHESTER_GOOP_DIR) && goop install
+
