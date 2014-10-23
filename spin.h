7ded9dd02cc33e083fc3e756c7ba48b4809afa18
Justin Flude <jflude@peak6.com>
2014-10-23 17:09:34 -0500

    typo

--- spin.h
+++ spin.h
@@ -15,7 +15,7 @@ typedef long spin_lock;
 #define SPIN_MIN LONG_MIN
 #define SPIN_MAX LONG_MAX
 
-#define SPIN_MASK (0x80ul << (CHAR_BIT * (sizeof(spin_lock) - 1)))
+#define SPIN_MASK (0x80uL << (CHAR_BIT * (sizeof(spin_lock) - 1)))
 
 void spin_create(volatile spin_lock *lock);
 status spin_read_lock(volatile spin_lock *lock, spin_lock *old_rev);
