be22c92d9ffbc917bbe8ef1ad1f50c75d5633def
Justin Flude <jflude@peak6.com>
2014-11-03 09:34:31 -0600

    more portable host/network byte order macros

--- h2n2h.h
+++ h2n2h.h
@@ -4,13 +4,20 @@
 #define H2N2H_H
 
 #include <arpa/inet.h>
+#include <sys/types.h>
 
 #if __BYTE_ORDER == __BIG_ENDIAN
+
+#undef htonll
+#undef ntohll
 
 #define htonll(x) (x)
 #define ntohll(x) (x)
 
 #elif __BYTE_ORDER == __LITTLE_ENDIAN
+
+#undef htonll
+#undef ntohll
 
 #define htonll(x) __builtin_bswap64(x)
 #define ntohll(x) __builtin_bswap64(x)
