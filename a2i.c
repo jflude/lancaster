afc8d276b3dbb0d169f08840ee0e141341f5ade8
Justin Flude <jflude@peak6.com>
2014-11-11 13:40:07 -0600

    a2i validates arguments

--- a2i.c
+++ a2i.c
@@ -5,6 +5,9 @@
 
 status a2i(const char *text, const char *format, void *pnum)
 {
+	if (!text || !format || !pnum)
+		return error_invalid_arg("a2i");
+
 	errno = 0;
 	if (sscanf(text, format, pnum) == EOF)
 		return errno != 0 ? error_errno("sscanf")
