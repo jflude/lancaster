09f03b13af304acd0ad97d40596a54e84ec038c7
Mike Vander Pluym <mvanderpluym@peak6.com>
2016-01-30 17:42:12 -0600

    Changing goop url from github.com/nitrous-io/goop to github.com/karmakaze/goop.
    code.google.com was taken down, so we need to point at a fork that's not pointing to dependencies that were on code.google.com until the goop repo is fixed, too

--- go/GOOP.mk
+++ go/GOOP.mk
@@ -12,7 +12,7 @@ export PATH := $(CACHESTER_VENDOR_DIR)/bin:$(PATH)
 	git config --global url."git@github.peak6.net:".insteadOf https://github.peak6.net
 
 .getGoop: .ensurePrivateGithubSSH
-	go get github.com/nitrous-io/goop
+	go get github.com/karmakaze/goop
 	mkdir -p $(CACHESTER_VENDOR_DIR)
 
 .installGoop: .getGoop
