7b2a66a2245ca10513ef78c49a5e7dff3a9682ad
Justin Flude <jflude@users.noreply.github.com>
2018-12-06 22:37:37 -0600

    endian testing by autoconf

--- lancaster/h2n2h.h
+++ lancaster/h2n2h.h
@@ -8,27 +8,25 @@
 #ifndef H2N2H_H
 #define H2N2H_H
 
+#ifdef HAVE_CONFIG_H
+#include "config.h"
+#endif
+
 #include <arpa/inet.h>
 #include <sys/types.h>
 
 #undef htonll
 #undef ntohll
 
-#if __BYTE_ORDER == __BIG_ENDIAN
+#ifdef WORDS_BIGENDIAN
 
 #define htonll(x) (x)
 #define ntohll(x) (x)
 
-#elif __BYTE_ORDER == __LITTLE_ENDIAN
+#else
 
 #define htonll(x) __builtin_bswap64(x)
 #define ntohll(x) __builtin_bswap64(x)
-
-#else
-
-#ifndef MAKE_DEPEND
-#error unrecognised value for __BYTE_ORDER
-#endif
 
 #endif
 
