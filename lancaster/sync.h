e74832156583b85338cd3f707188a3deafa3dfe5
Justin Flude <jflude@users.noreply.github.com>
2018-12-08 19:23:49 -0600

    check for compiler __sync intrinsics

--- lancaster/sync.h
+++ lancaster/sync.h
@@ -12,9 +12,13 @@
 #include "config.h"
 #endif
 
+#ifdef HAVE_SYNC_INTRINSICS
 #define SYNC_FETCH_AND_OR __sync_fetch_and_or
 #define SYNC_LOCK_RELEASE __sync_lock_release
 #define SYNC_SYNCHRONIZE __sync_synchronize
+#else
+#error no definitions for synchronization intrinsics
+#endif
 
 #if defined(X86_64_CPU)
 #define CPU_RELAX(x) __asm__ __volatile__("pause" ::: "memory")
