d30bafee7437966fe17d691287ab74b5efe3c791
Justin Flude <jflude@users.noreply.github.com>
2024-08-26 01:46:36 -0500

    copyright notices where appropriate

--- src/go/src/commander/nodeath.go
+++ src/go/src/commander/nodeath.go
@@ -2,7 +2,6 @@
 
 /*
    Copyright (c)2014-2017 Peak6 Investments, LP.
-   Copyright (c)2018-2024 Justin Flude.
    Use of this source code is governed by the COPYING file.
 */
 
--- src/go/src/pubmux/main.go
+++ src/go/src/pubmux/main.go
@@ -1,6 +1,5 @@
 /*
    Copyright (c)2014-2017 Peak6 Investments, LP.
-   Copyright (c)2018-2024 Justin Flude.
    Use of this source code is governed by the COPYING file.
 */
 
